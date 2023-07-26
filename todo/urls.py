49ae631947bb9e1fe9a7952b2112a60fee0268b0
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-26 12:47:07 +0300

    Fix home page name

--- todo/urls.py
+++ todo/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("todo_list.urls", namespace="todo list"))
+    path("", include("todo_list.urls", namespace="todo_list"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
