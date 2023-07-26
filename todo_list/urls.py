a691f97cddf52f6ad6f36cd9c8810909d2b870c7
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-26 13:52:02 +0300

    Add crud for tasks

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,24 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
 
-from todo_list.views import HomeView
+from todo_list.views import (
+    HomeView,
+    TaskUpdateView,
+    TaskCreateView,
+    TaskDeleteView
+)
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home")
+    path(
+        "",
+        RedirectView.as_view(
+            url=reverse_lazy("todo_list:home")
+        )
+    ),
+    path("task/", HomeView.as_view(), name="home"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete")
 ]
 
 app_name = "todo_list"
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from todo_list.forms import TaskForm
 from todo_list.models import Task
 
 
@@ -8,3 +9,19 @@ class HomeView(generic.ListView):
     model = Task
     paginate_by = 5
 
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:home")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:home")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo_list:home")
